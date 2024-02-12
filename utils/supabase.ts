c862ce55d3f01887824ebf3a5054bada680baf97
yanahirogit config --global user.email yanahiro0214@gmail.com <yanahiro0214@gmail.com>
2024-02-12 16:50:40 +0900

    デプロイします

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,10 +1,6 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON;
-
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error("Missing SUPABASE_URL or SUPABASE_ANON environment variable");
-}
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export default createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+)
