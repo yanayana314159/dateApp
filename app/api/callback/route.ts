22de12226cd6632a8bad6c8f18d260e563c07413
yanahirogit config --global user.email yanahiro0214@gmail.com <yanahiro0214@gmail.com>
2024-02-13 17:07:48 +0900

    projectBaseURLの変更

--- app/api/callback/route.ts
+++ app/api/callback/route.ts
@@ -4,13 +4,12 @@ import { cookies } from "next/headers";
 import { NextResponse, type NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
-  const projectBaseUrl = process.env.NEXT_PUBLIC_PROJECT_BASE_URL;
   const requestUrl = new URL(request.url);
+  const projectBaseUrl = requestUrl.pathname.split("/api")[0];
   const code = requestUrl.searchParams.get("code");
   if (code) {
     const supabase = createRouteHandlerClient({ cookies });
     await supabase.auth.exchangeCodeForSession(code);
   }
-  alert(projectBaseUrl)
-  return NextResponse.redirect(`https://date-app-opal.vercel.app/userpage/home`);
+  return NextResponse.redirect(`${projectBaseUrl}/userpage/home`);
 }
