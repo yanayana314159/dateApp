c714a20bf5bcf19bcd40554eeb5311caed12d8b3
yanahirogit config --global user.email yanahiro0214@gmail.com <yanahiro0214@gmail.com>
2024-02-13 16:43:02 +0900

    本番と開発環境でredirect URLの変更

--- app/api/callback/route.ts
+++ app/api/callback/route.ts
@@ -4,11 +4,12 @@ import { cookies } from "next/headers";
 import { NextResponse, type NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
+  const projectBaseUrl = process.env.NEXT_PUBLIC_PROJECT_BASE_URL;
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
   if (code) {
     const supabase = createRouteHandlerClient({ cookies });
     await supabase.auth.exchangeCodeForSession(code);
   }
-  return NextResponse.redirect("http://localhost:3000/userpage/home");
+  return NextResponse.redirect(`https://date-app-opal.vercel.app/userpage/home`);
 }
